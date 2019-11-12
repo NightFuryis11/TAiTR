63f35b72f8fd6471355f7aab6d6489b111a8073e
NightFuryis11 <43391635+NightFuryis11@users.noreply.github.com>
2019-11-12 11:26:45 -0500

    Cleaned Bean.cs
    Removed ExampleMod comments.

--- Items/Ammo/Bean.cs
+++ Items/Ammo/Bean.cs
@@ -17,16 +17,15 @@ namespace TAiTR.Items.Ammo
 			item.width = 8;
 			item.height = 8;
 			item.maxStack = 999;
-			item.consumable = true;             //You need to set the item consumable so that the ammo would automatically consumed
+			item.consumable = true;
 			item.knockBack = 1.5f;
 			item.value = 10;
 			item.rare = 2;
-			item.shoot = mod.ProjectileType("Bean");   //The projectile shoot when your weapon using this ammo
-			item.shootSpeed = 16f;                  //The speed of the projectile
-			item.ammo = item.type;              //The ammo class this ammo belongs to.
+			item.shoot = mod.ProjectileType("Bean");
+			item.shootSpeed = 16f;
+			item.ammo = item.type;
 		}
 
-		// Give each bullet consumed a 20% chance of granting the Wrath buff for 5 seconds
 		public override void AddRecipes() {
 			ModRecipe recipe = new ModRecipe(mod);
 			recipe.AddIngredient(ItemID.MusketBall, 50);
