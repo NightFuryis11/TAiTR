73d1ade0a16f78e710c0ebf7265a3df3024e5fe7
NightFuryis11 <43391635+NightFuryis11@users.noreply.github.com>
2019-11-12 12:31:52 -0500

    Update BreadstickSword.cs

--- Items/Weapons/BreadstickSword.cs
+++ Items/Weapons/BreadstickSword.cs
@@ -21,7 +21,7 @@ namespace TAiTR.Items.Weapons
 			item.useStyle = 1;
 			item.knockBack = 6;
 			item.value = 10;
-			item.rare = 2;
+			item.rare = 0;
 			item.UseSound = SoundID.Item1;
 			item.autoReuse = false;
 		}
